50216e05739707f0264e9b16a3ec322099300180
ryandickersonnn <dickerson.r@wustl.edu>
2023-10-14 22:13:10 -0500

    asfjhadskljhf

--- main.js
+++ main.js
@@ -25,17 +25,54 @@ async function main() {
             // console.log(assignment.assignment_name);   // This will log the details of the course
         }
     }
+    const now = new Date();
     let question = "";
+    // let dated = new Map();
+    const dated = {};
+
+
+    // for(let i = 0; i < dueDates.length; i++){
+    //     if(dueDates[i] != null){
+    //         const fixedDate = dueDates[i].replace(/[TZ]/g, ' ');
+    //         const parts = fixedDate.split(" ");
+    //         let dueDate = new Date(parts[0]);
+    //         if(dueDate > now){
+    //             question += "Assignment: ";
+    //             question += names[i];
+    //             question += " Due Date: ";
+    //             question += parts[0];
+    //             question += " Due Time: ";
+    //             question += parts[1];
+    //             question += " ";
+
+    //         }
+    //     }
+    // }
     for(let i = 0; i < dueDates.length; i++){
         if(dueDates[i] != null){
-            question += names[i];
-            question += " ";
             const fixedDate = dueDates[i].replace(/[TZ]/g, ' ');
-            question += fixedDate;
-            question += " ";
+            const parts = fixedDate.split(" ");
+            let dueDate = new Date(parts[0]);
+            dated[dueDate] = [names[i], parts[0]];
+            }
         }
+    // for(key in dated){
+    //     console.log(dated[key]);
+    // }
+    const keysArray = Object.keys(dated);
+    keysArray.sort();
+    // let values = dated.keys();
+    // sortedKeys = values.sort();
+    console.log(keysArray);
+    for(key in keysArray){
+        console.log(dated[key])
     }
-    console.log(question);
+    // const sortedMap = new Map([Object.keys(dated)].sort());
+    // console.log(sortedMap)
+    // for (var i = 0, keys = Object.keys(sortedMap), ii = keys.length; i < ii; i++) {
+    //     console.log(keys[i] + '|' + sortedMap[keys[i]]);
+    //   }
+    // console.log(question);
 }
 
 main();
