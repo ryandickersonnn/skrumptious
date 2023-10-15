85223d8883a2bfd13c6ce5dc6e09a05e9a26886d
Ken Wilbur <68517811+ken-wilbur@users.noreply.github.com>
2023-10-14 19:16:04 -0500

    Fixed openAI API problems

--- openaiaccess.js
+++ openaiaccess.js
@@ -1,41 +1,38 @@
 const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config({ path: '.env.local' });
 
 const config = new Configuration({
-	apiKey: "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",
+    apiKey: process.env.OPENAI_API_KEY
 });
 
 const openai = new OpenAIApi(config);
 
-let data = "";
-for(let i = 0; i < events.length(); i++){
-    data += envents[i];
-}
-
-
-
 const runPrompt = async () => {
-	const prompt = `
-        write me a joke about a cat and a bowl of pasta. Return response in the following parsable JSON format:
+    try {
+        const prompt = `
+            write me a joke about a cat and a bowl of pasta. Return response in the following parsable JSON format:
 
-        {
-            "Q": "question",
-            "A": "answer"
-        }
+            {
+                "Q": "question",
+                "A": "answer"
+            }
+        `;
 
-    `;
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 2048,
+            temperature: 1,
+        });
 
-	const response = await openai.createCompletion({
-		model: "text-davinci-003",
-		prompt: prompt,
-		max_tokens: 2048,
-		temperature: 1,
-	});
+        const parsableJSONresponse = response.data.choices[0].text;
+        const parsedResponse = JSON.parse(parsableJSONresponse);
 
-	const parsableJSONresponse = response.data.choices[0].text;
-	const parsedResponse = JSON.parse(parsableJSONresponse);
-
-	console.log("Question: ", parsedResponse.Q);
-	console.log("Answer: ", parsedResponse.A);
+        console.log("Question: ", parsedResponse.Q);
+        console.log("Answer: ", parsedResponse.A);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
 };
 
 runPrompt();
