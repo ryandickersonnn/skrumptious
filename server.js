29e683d797b53bc302ecead422252227ce4cbd49
ryandickersonnn <dickerson.r@wustl.edu>
2023-10-15 09:05:06 -0500

    no more

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
+ 
+const hostname = '127.0.0.1';
+const port = 3000;
+ 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'test.html'); // Replace with your HTML file's path
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404 Not Found');
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      res.end();
-    }
-  });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
-
-server.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000/');
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
